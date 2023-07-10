5de3fa40be94b32cf86979cdfb85157ad651442c
hayoniii <hayeon7410@naver.com>
2023-07-10 11:42:14 +0900

    commit5

--- DAY4/JK_FF/testbench/testbench.v
+++ DAY4/JK_FF/testbench/testbench.v
@@ -2,7 +2,6 @@
 `define T_CLK 10
 module testbench;
     reg clk;
-    reg n_rst;
     reg J;
     reg K;
     wire Q;
@@ -10,7 +9,6 @@ module testbench;
 
     jk_ff u_jk_ff(
         .clk(clk),
-        .n_rst(n_rst),
         .J(J),
         .K(K),
         .Q(Q),
@@ -21,12 +19,9 @@ module testbench;
 
     initial begin
         clk = 1'b1;
-        n_rst = 1'b0;
-        #(`T_CLK*1.1)n_rst = 1'b1;
     end
  
     initial begin
-        wait(n_rst == 1'b1);
         J = 0;
         K = 0;
         #10;
