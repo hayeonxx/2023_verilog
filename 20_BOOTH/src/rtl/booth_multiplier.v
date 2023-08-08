fb834949efbc8260945fcc62b0a11f8ef8520a33
hayoniii <hayeon7410@naver.com>
2023-08-08 16:56:01 +0900

    commit

--- 20_BOOTH/src/rtl/booth_multiplier.v
+++ 20_BOOTH/src/rtl/booth_multiplier.v
@@ -7,13 +7,13 @@ module booth_multiplier(
     output reg [7:0] result
 );
 
-//reg state;
-//reg n_state;
+reg state;
+reg n_state;
 reg [2:0] cnt;
 reg [9:0] A;
 reg [9:0] S;
 reg [9:0] P;
-/*
+
 localparam IDLE = 2'h0;
 localparam CAL = 2'h1;
 
@@ -31,12 +31,12 @@ always @(*) begin
         default: n_state = IDLE;
     endcase
 end
-*/
+
 always@(posedge clk or negedge n_rst) begin
     if(!n_rst)
         cnt <= 3'h0;
     else begin
-        if(start == 1'b1)
+        if(state == IDLE)
             cnt <= 3'h0;
         else
             cnt <= (cnt == 3'h4) ? 3'h0 : cnt + 3'h1;
@@ -47,10 +47,10 @@ always @(posedge clk or negedge n_rst) begin
     if(!n_rst)
         A <= 10'b0_0000_0000_0;
     else begin
-        if(start == 1'b1)
-            A <= {M, A[4:0]};
+        if(state == IDLE)
+            A <= 10'b0_0000_0000_0;
         else
-            A <= A;
+            A <= {M, A[4:0]};
     end
 end
 
@@ -58,31 +58,31 @@ always @(posedge clk or negedge n_rst) begin
     if(!n_rst)
         S <= 10'b0_0000_0000_0;
     else begin
-        if(start == 1'b1)
-            S <= {(~M + 1'b1), A[4:0]};
+        if(state == IDLE)
+            S <= 10'b0_0000_0000_0;
         else
-            S <= S;
+            S <= {(~M + 1'b1), A[4:0]};
     end
 end
-/*
+
 always @(posedge clk or negedge n_rst) begin
     if(!n_rst)
         P <= 10'b0_0000_0000_0;
     else begin
-        if(start == 1'b1)
+        if(state == IDLE)
             P <= 10'b0_0000_0000_0;
         else
             P <= {P[9:6], Q, 1'b0};
     end
 end 
-*/
+
 reg [1:0] P_LSB;
 
 always @(posedge clk or negedge n_rst) begin
     if(!n_rst) 
-        P_LSB <= 2'b00;
+        P_LSB <= P[1:0];
     else begin
-        if(cnt <= 3'h3) 
+        if(state == CAL) 
             P_LSB <= P[1:0];
         else
             P_LSB <= P_LSB;
@@ -94,36 +94,27 @@ always @(posedge clk or negedge n_rst) begin
         P <= 10'b0_0000_0000_0;
     end
     else begin
-        if (start == 1'b1)
-            P <= {P[9:6], Q, 1'b0};
-        else if(cnt <= 3'h3)
-            P <= P <= {1'b0, P[9:1]};
-        else
-            P <= P;
+        if(state == IDLE) begin
+            P <= 10'b0_0000_0000_0;
+        else begin
+            case(P_LSB)
+                2'b01: P = P + A;
+                2'b10: P = P + S;
+                default: P = P;
+            endcase
+        end
+    end
+    P <= {1'b0, P[9:1]};
     end
 end
 
-always @(posedge clk or negedge n_rst) begin
-    if(P_LSB == 01)
-        P <= P + A;
-    else if(P_LSB == 10)
-        P <= P + S;
-    else 
-        P <= P;
-    /*case(P_LSB)
-        2'b01: P = P + A;
-        2'b10: P = P + S;
-        default: P = P;
-    endcase
-    */
-end
-  
+
 
 always @(posedge clk or negedge n_rst) begin
     if(!n_rst)
         result <= 8'b0000_0000;
     else begin
-        if(cnt <= 3'h4) 
+        if(state == CAL) 
             result <= P[8:1];
         else 
             result <= result;
